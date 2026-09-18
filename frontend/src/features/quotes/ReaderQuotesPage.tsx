bdc452232c03ec3b07a4702b4528bf470659acb3
Usman <usman@MacBook-Pro-Usman.local>
2026-09-19 03:01:20 +0500

    Remove top quotation mark icon from quote cards

--- frontend/src/features/quotes/ReaderQuotesPage.tsx
+++ frontend/src/features/quotes/ReaderQuotesPage.tsx
@@ -190,27 +190,8 @@ export const ReaderQuotesPage: React.FC = () => {
                   position: 'relative',
                 }}
               >
-                {/* Top quote icon */}
+                {/* Top quote content */}
                 <div>
-                  <div
-                    style={{
-                      width: '36px',
-                      height: '36px',
-                      borderRadius: '10px',
-                      background: 'rgba(0, 84, 148, 0.08)',
-                      color: 'var(--blue)',
-                      display: 'flex',
-                      alignItems: 'center',
-                      justifyContent: 'center',
-                      marginBottom: '16px',
-                    }}
-                  >
-                    <svg width="18" height="18" viewBox="0 0 24 24" fill="currentColor">
-                      <path d="M3 21c3 0 7-1 7-8V5c0-1.25-.756-2.017-2-2H4c-1.25 0-2 .75-2 1.972V11c0 1.25.75 2 2 2 1 0 1 0 1 1v1c0 1-1 2-2 2s-1 .008-1 1.031V20c0 1 0 1 1 1z" />
-                      <path d="M15 21c3 0 7-1 7-8V5c0-1.25-.757-2.017-2-2h-4c-1.25 0-2 .75-2 1.972V11c0 1.25.75 2 2 2 1 0 1 0 1 1v1c0 1-1 2-2 2s-1 .008-1 1.031V20c0 1 0 1 1 1z" />
-                    </svg>
-                  </div>
-
                   {/* Quote text */}
                   <p
                     style={{
